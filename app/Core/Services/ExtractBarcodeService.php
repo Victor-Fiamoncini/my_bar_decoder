0191cd05618880ef5512a17c9d4324f04d1019b6
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-12-23 18:43:11 -0300

    feat: enhance ExtractBarcodeService to create Document on successful payment code extraction and throw exception on failure

--- app/Core/Services/ExtractBarcodeService.php
+++ app/Core/Services/ExtractBarcodeService.php
@@ -2,17 +2,32 @@
 
 namespace App\Core\Services;
 
-use App\Core\DTOs\FileDTO;
 use App\Core\Services\Contracts\FileBarcodeExtractor;
+use App\Core\Services\DTOs\FileDTO;
+use App\Core\Services\Exceptions\ExtractPaymentCodeException;
+use App\Models\Document;
 
 readonly class ExtractBarcodeService
 {
     public function __construct(private FileBarcodeExtractor $fileBarcodeExtractor) {}
 
-    public function execute(FileDTO $fileDTO): ?string
+    /**
+     * @throws ExtractPaymentCodeException
+     */
+    public function execute(FileDTO $fileDTO): string
     {
         $paymentCode = $this->fileBarcodeExtractor->extractFromFilePath($fileDTO->path);
 
-        return $paymentCode;
+        if ($paymentCode) {
+            Document::create([
+                'name' => $fileDTO->name,
+                'code' => $paymentCode,
+                'user_id' => auth()->id(),
+            ]);
+
+            return $paymentCode;
+        }
+
+        throw new ExtractPaymentCodeException();
     }
 }
