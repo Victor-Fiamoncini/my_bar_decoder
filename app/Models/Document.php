68a54f397c9352fdf66a5d6e2e66ec2091489078
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-12-23 18:35:59 -0300

    feat: add soft deletes to Document model and create migration for documents table

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -6,15 +6,16 @@ use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Document extends Model
 {
-    use HasFactory, HasTimestamps;
+    use HasFactory, HasTimestamps, SoftDeletes;
 
     protected $fillable = [
-        'user_id',
-        'filename',
+        'name',
         'code',
+        'user_id',
     ];
 
     public function user(): BelongsTo
