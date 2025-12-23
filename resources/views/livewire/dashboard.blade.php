f702ef79a5a9d137ac2f6178343ca89d55d94005
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-12-23 18:43:16 -0300

    feat: enhance ExtractBarcodeService to create Document on successful payment code extraction and throw exception on failure

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -2,29 +2,35 @@
     <div class="max-w-lg w-full space-y-6">
         <flux:heading>{{ __('Bar Decoder') }}</flux:heading>
 
-        <flux:subheading>{{ __('Upload a bill document/image and we will extract its barcode.') }}</flux:subheading>
+        <flux:subheading>{{ __('Upload a bill PDF document and we will extract its payment code.') }}</flux:subheading>
 
-        <form class="space-y-4" wire:submit="submit">
-            <input
-                class="block w-full cursor-pointer"
-                type="file"
-                wire:model="file"
-                accept="image/png,image/jpeg,application/pdf"
-            />
+        <form class="space-y-2 mb-6" wire:submit="submit">
+            <div class="flex items-center justify-start">
+                <flux:icon.document-arrow-up />
 
-            <div class="flex items-center justify-end">
-                <flux:button class="cursor-pointer" type="submit" variant="primary" data-test="decode-bill-button">
-                    {{ __('Extract Barcode') }}
+                <flux:input.file
+                    class="block w-full text-md cursor-pointer p-3 rounded-md"
+                    wire:model="file"
+                    type="file"
+                    accept="image/png,image/jpeg,application/pdf"
+                    data-test="file-input"
+                />
+            </div>
+
+            <div class="flex items-center justify-start">
+                <flux:button
+                    class="cursor-pointer"
+                    type="submit"
+                    variant="primary"
+                    data-test="submit-button"
+                >
+                    {{ __('Extract Payment Code') }}
                 </flux:button>
             </div>
         </form>
 
-        @if ($barcode)
-            <flux:heading size="lg">{{ __('Barcode') }}: {{ $barcode }}</flux:heading>
+        @if ($paymentCode)
+            <flux:heading size="md">{{ __('Extracted Payment Code') }}: {{ $paymentCode }}</flux:heading>
         @endif
-    </div>
-
-    <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-        <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
     </div>
 </div>
