72c25e156845943b86833f124d2544b1d10b0fb2
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-12-24 07:16:03 -0300

    feat: enhance Dashboard mobile layout and add clipboard functionality for payment codes

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,4 +1,4 @@
-<div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+<div class="flex h-full w-full flex-1 flex-col gap-4">
     <div class="w-full space-y-6 mb-6">
         <flux:heading size="lg">{{ __('Bar Decoder') }}</flux:heading>
 
@@ -9,7 +9,7 @@
                 <flux:icon.document-arrow-up />
 
                 <flux:input.file
-                    class="block w-full text-md cursor-pointer p-3 rounded-md"
+                    class="block text-md cursor-pointer p-3 rounded-md"
                     wire:model="file"
                     type="file"
                     accept="image/png,image/jpeg,application/pdf"
@@ -30,10 +30,14 @@
         </form>
 
         @if ($paymentCode)
-            <div class="p-6 border border-green-200 rounded-lg w-full" data-test="payment-code-card">
+            <div class="p-6 border border-green-200 rounded-lg space-y-2" data-test="payment-code-card">
                 <flux:heading>{{ __('Extracted Payment Code') }}</flux:heading>
 
-                <flux:subheading>{{ $paymentCode }}</flux:subheading>
+                <div class="flex items-center justify-start gap-2 flex-wrap">
+                    <livewire:components.clipboard-button :text="$paymentCode" />
+
+                    <flux:subheading class="break-all">{{ $paymentCode }}</flux:subheading>
+                </div>
             </div>
         @endif
     </div>
@@ -45,10 +49,10 @@
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 @foreach ($documents as $document)
                     <div class="p-6 border rounded-lg w-full">
-                        <div class="space-y-2">
-                            <flux:heading size="sm">{{ $document->name }}</flux:heading>
+                        <div class="space-y-2" data-test="document-card">
+                            <flux:heading class="wrap-break-word">{{ $document->name }}</flux:heading>
 
-                            <flux:subheading class="text-sm">{{ $document->code }}</flux:subheading>
+                            <flux:subheading class="break-all">{{ $document->code }}</flux:subheading>
 
                             <flux:badge size="sm" variant="subtle">
                                 {{ $document->created_at->format('M d, Y H:i') }}
