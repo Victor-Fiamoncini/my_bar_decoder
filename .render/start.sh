ca60bed69309c9eb7bc1e44726e573559aad42ea
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-12-28 19:50:51 -0300

    feat: streamline asset publishing in Dockerfile and start script

--- .render/start.sh
+++ .render/start.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-php artisan config:clear
-php artisan cache:clear
-php artisan view:clear
-
-# Republish assets to ensure they're available
-php artisan livewire:publish --assets
-php artisan vendor:publish --tag=flux-assets --force
-
 php artisan migrate --force
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
 php-fpm -D
 
