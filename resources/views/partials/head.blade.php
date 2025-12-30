6e926e09ce71d256ac1c98c2f06b52914e0591e6
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-12-30 18:20:58 -0300

    feat: update favicon links and add SVG favicon for improved branding and compatibility

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -7,9 +7,10 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon.png') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicon-96x96.png') }}">
+<link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
