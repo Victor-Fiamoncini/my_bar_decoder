7385d56439aef665c76750ed5a2a72c6a574eb5b
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-12-30 16:58:49 -0300

    feat: update app logo implementation and improve accessibility with title attributes

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,5 +1,5 @@
 <div class="flex items-center gap-1">
-    <img class="w-12 h-10" src="{{ asset('logo.png') }}" alt="App Logo"  />
+    <img class="w-12 h-10" src="{{ asset('logo.png') }}" alt="App Logo" title="My Bar Decoder" />
 
     <div class="grid flex-1 text-start text-sm">
         <span class="truncate leading-tight font-semibold">My Bar Decoder</span>
