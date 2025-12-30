c7f42a8eaf999df96c2cb3906d62f1f9af406646
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-12-30 16:14:20 -0300

    feat: update head.blade.php to enhance favicon and theme color support

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,11 +1,15 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+<meta name="theme-color" content="#5e72e400" />
+<meta name="msapplication-navbutton-color" content="#5e72e400" />
+<meta name="apple-mobile-web-app-status-bar-style" content="#5e72e400" />
+
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon.png') }}">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
